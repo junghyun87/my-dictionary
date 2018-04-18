9e1376bb4ccf35036e35e82d85aa0c76b3e97df7
junghyun <arunson@kaist.ac.kr>
2018-04-18 10:34:55 +0900

    fix: show the mobile pages of some webpages

--- src/renderer/Dic.jsx
+++ src/renderer/Dic.jsx
@@ -112,6 +112,7 @@ class Dic extends Component {
           <webview
             ref="_webview"
             src={this.state.webview_url}
+            useragent="Mozilla/5.0 (Linux; Android 5.1.1; Nexus 5 Build/LMY48B; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.65 Mobile Safari/537.36"
             style={wv_style}
           />
         </div>
